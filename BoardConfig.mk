325e63be586127a1118e9cf8fe1deadd3b77f66d
singh_dd93 <singh_dd93@yahoo.in>
2013-04-14 12:04:02 +0530

    Upbring to latest semc7x30 source
    [*] Moved torch overlay to msm7x30
    [*] Added ALS
    [*] Use nAa kernel
    
    Change-Id: I7b62e37310592ef2844acc115ccfd4747d4563bf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,32 +1,20 @@
-# This variable is set first, so it can be overridden
-# by BoardConfigVendor.mk
 -include device/semc/mogami-common/BoardConfigCommon.mk
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/bluetooth
+-include vendor/semc/urushi/BoardConfigVendor.mk
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
-TARGET_KERNEL_CONFIG := pac_urushi_defconfig
+TARGET_KERNEL_CONFIG := nAa_urushi_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-gnueabihf-linaro-4.7
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/urushi/recovery/init.rc
 
-# Sensor
+# Sensors
 SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 
-# Assert
 TARGET_OTA_ASSERT_DEVICE := ST18i,ST18a,urushi
 
-#twrp
-DEVICE_RESOLUTION := 480x854
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_FLASH_FROM_STORAGE := true
-
-# inherit from the proprietary version
--include vendor/semc/urushi/BoardConfigVendor.mk
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/bluetooth
--- urushi.mk
+++ urushi.mk
@@ -9,7 +9,6 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
 PRODUCT_AAPT_CONFIG := normal hdpi hdpi
@@ -18,7 +17,6 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Init files
 PRODUCT_COPY_FILES += \
     device/semc/urushi/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
-    device/semc/urushi/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/urushi/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle
 
@@ -41,7 +39,7 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
 
-#LCD DENSITY
+# Device properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
     com.qc.hdmi_out=false
